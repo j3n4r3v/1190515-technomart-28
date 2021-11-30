782278c199c036dcd27e931faadfe1afbeba18cb
j4r <jene4ka4reva@gmail.com>
2021-11-30 14:39:10 +0200

    fix changes

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -1,65 +1,55 @@
-var isIndex = document.body.classList.contains("js-index");
+const buyLinks = document.querySelectorAll(".buy__button");
+const buyPopup = document.querySelector(".added__cart");
+const buyContinue = buyPopup.querySelector(".button-checkout");
+const buyClose = buyPopup.querySelector(".modal__close-cart");
+const chosenProducts = document.querySelector(".cart__count");
+let chosenProductsQuantity = 10;
+const countBuyProducts = document.querySelector(".buy-card__count");
 
-var buyLinks = document.querySelectorAll(".buy__button");
+const addToBookmarksButtons = document.querySelectorAll(".bookmarks__button");
+const chosenBookmarksProducts = document.querySelector(".bookmark__count");
+const countBookmarkProducts = document.querySelector(".bookmark-card__count");
+let bookmarksQuantity = 0;
 
-var buyPopup = document.querySelector(".added__cart");
-var buyContinue = buyPopup.querySelector(".button-checkout");
-var buyClose = buyPopup.querySelector(".modal__close-cart");
-
-var chosenProducts = document.querySelector(".cart__count");
-var chosenProductsQuantity = 0;
-
-function closePopup (popup) {
-
+const closePopup = (popup) => {
   popup.classList.remove("modal-show");
 }
-
-if(!isIndex) {
-  chosenProductsQuantity = 10;
-  putProductToCart();
-}
-
-function putProductToCart () {
+const putProductToCart = () => {
   chosenProducts.classList.add("btn-cart-not-empty");
-  chosenProducts.innerHTML = "Корзина: " + chosenProductsQuantity;
+  countBuyProducts.textContent = chosenProductsQuantity;
 }
 
-for (i = 0; i < buyLinks.length; i++) {
-  buyLinks[i].addEventListener("click", function (evt) {
+buyLinks.forEach((item) => {
+  item.addEventListener("click", (evt) => {
     evt.preventDefault();
     buyPopup.classList.add("modal-show");
-
     chosenProductsQuantity++;
     putProductToCart();
   });
-}
+})
 
-buyContinue.addEventListener("click", function (evt) {
+buyContinue.addEventListener("click", (evt) => {
   closePopup(buyPopup);
 });
 
-buyClose.addEventListener("click", function (evt) {
+buyClose.addEventListener("click", (evt) => {
   evt.preventDefault();
   buyPopup.classList.remove("modal-show");
 });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+window.addEventListener("keydown", (evt) => {
+  if (evt.code === "Escape") {
     if (buyPopup.classList.contains("modal-show")) {
       closePopup(buyPopup);
     }
   }
 });
 
-var bookmarks = document.querySelector(".bookmark__count");
-var addToBookmarksButtons = document.querySelectorAll(".bookmarks__button");
-var bookmarksQuantity = 0;
-
-for (i = 0; i < addToBookmarksButtons.length; i++) {
-  addToBookmarksButtons[i].addEventListener("click", function (evt) {
+addToBookmarksButtons.forEach((item) => {
+  item.addEventListener("click", (evt) => {
     evt.preventDefault();
     bookmarksQuantity++;
-    bookmarks.classList.add("btn-bookmark-not-empty");
-    bookmarks.innerHTML = "Закладки: " + bookmarksQuantity;
+    chosenBookmarksProducts.classList.add("btn-bookmark-not-empty");
+    countBookmarkProducts.textContent= bookmarksQuantity;
   });
-}
+})
